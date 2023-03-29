5414ab897fd7c146e56e5ef3a1bfdf57de2a1ef1
jonathanochoasurya <81263926+jonathanochoasurya@users.noreply.github.com>
2023-03-29 15:09:14 -0400

    Unit Test for all Calculator Operations

--- NUnitTest/UnitTest1.cs
+++ NUnitTest/UnitTest1.cs
@@ -1,19 +1,75 @@
 using Calculator;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Serialization;
+using NUnit.Framework.Constraints;
+using NUnit.Framework.Internal;
 
 namespace NUnitTest
 {
     public class Tests
     {
+        //Arrange
+        public double a = 6;
+        public double b = 3;
+
         [SetUp]
         public void Setup()
         {
-            Calculate testC1 = new Calculate();
         }
 
         [Test]
-        public void Test1()
+        public void WhenAddTwoNumbers_ThenReturnSumOfTwoNumbers()
         {
-            Assert.Pass();
+
+            var expected = 9;
+
+            //Actual
+            var actual = Calculate.DoOperation(a, b, "a");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void WhenSubtractTwoNumbers_ThenReturnFirstNumberMinusSecondNumber()
+        {
+
+            var expected = 3;
+
+            //Actual
+            var actual = Calculate.DoOperation(a, b, "b");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void WhenMultiplyTwoNumbers_ThenReturnMultiplicationOfTwoNumbers()
+        {
+
+            var expected = 18;
+
+            //Actual
+            var actual = Calculate.DoOperation(a, b, "c");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void WhenDivideTwoNumbers_ThenReturnDivisionOfTwoNumbers()
+        {
+
+            var expected = 2;
+
+            //Actual
+            var actual = Calculate.DoOperation(a, b, "d");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
         }
     }
 }
